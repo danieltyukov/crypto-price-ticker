16a2fc44dac43a12d6fe504f6a74e56fa26fb879
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-08-18 16:31:43 +0200

    final changes

--- ticker.py
+++ ticker.py
@@ -38,36 +38,36 @@ def get_data():
 
     coin_name = []
     coin_symbol = []
-    market_cap = []
-    percent_change_1h = []
-    percent_change_24h = []
-    percent_change_7d = []
+    marketCap = []
+    percentChange1h = []
+    percentChange24h = []
+    percentChange7d = []
     price = []
-    volume_24h = []
+    volume24h = []
 
     for i in listing:
         coin_name.append(i['slug'])
         coin_symbol.append(i['symbol'])
         price.append(i['quote'][price_unit]['price'])
-        percent_change_1h.append(i['quote'][price_unit]['percent_change_1h'])
-        percent_change_24h.append(i['quote'][price_unit]['percent_change_24h'])
-        percent_change_7d.append(i['quote'][price_unit]['percent_change_7d'])
-        market_cap.append(i['quote'][price_unit]['market_cap'])
-        volume_24h.append(i['quote'][price_unit]['volume_24h'])
+        percentChange1h.append(i['quote'][price_unit]['percentChange1h'])
+        percentChange24h.append(i['quote'][price_unit]['percentChange24h'])
+        percentChange7d.append(i['quote'][price_unit]['percentChange7d'])
+        marketCap.append(i['quote'][price_unit]['marketCap'])
+        volume24h.append(i['quote'][price_unit]['volume24h'])
 
     df = pd.DataFrame(columns=[
-        'coin_name', 'coin_symbol', 'market_cap', 'percent_change_1h',
-        'percent_change_24h', 'percent_change_7d', 'price', 'volume_24h'
+        'coin_name', 'coin_symbol', 'marketCap', 'percentChange1h',
+        'percentChange24h', 'percentChange7d', 'price', 'volume24h'
     ])
 
     df['coin_name'] = coin_name
     df['coin_symbol'] = coin_symbol
     df['price'] = price
-    df['percent_change_1h'] = percent_change_1h
-    df['percent_change_24h'] = percent_change_24h
-    df['percent_change_7d'] = percent_change_7d
-    df['market_cap'] = market_cap
-    df['volume_24h'] = volume_24h
+    df['percentChange1h'] = percentChange1h
+    df['percentChange24h'] = percentChange24h
+    df['percentChange7d'] = percentChange7d
+    df['marketCap'] = marketCap
+    df['volume24h'] = volume24h
     return df
 
 
@@ -79,14 +79,14 @@ select_by_coin = col.multiselect('Cryptocurrency', sort_by_coin, sort_by_coin)
 df_select_by_coin = df[(df['coin_symbol'].isin(select_by_coin))]
 
 number_of_coins = col.slider('Display the top N amount of coins', 1, 100, 100)
-df_coins = df_select_by_coin.head[:number_of_coins]
+df_coins = df_select_by_coin[:number_of_coins]
 
 percentage_time_frame = col.selectbox('Percentage change of time frame',
                                       ['1h', '24h', '7d'])
 percentage_dict = {
-    '1h': 'percent_change_1h',
-    '24h': 'percent_change_24h',
-    '7d': 'percent_change_7d'
+    '1h': 'percentChange1h',
+    '24h': 'percentChange24h',
+    '7d': 'percentChange7d'
 }
 selected_percentage_time_frame = percentage_dict[percentage_time_frame]
 
@@ -110,14 +110,14 @@ col1.markdown(filedownload(df_select_by_coin), unsafe_allow_html=True)
 
 col1.subheader('Table of the percentage price change')
 df_change = pd.concat([
-    df_coins.coin_symbol, df_coins.percent_change_1h,
-    df_coins.percent_change_24h, df_coins.percent_change_7d
+    df_coins.coin_symbol, df_coins.percentChange1h, df_coins.percentChange24h,
+    df_coins.percentChange7d
 ],
                       axis=1)
 df_change = df_change.set_index('coin_symbol')
-df_change['positive_percent_change_1h'] = df_change['percent_change_1h'] > 0
-df_change['positive_percent_change_24h'] = df_change['percent_change_24h'] > 0
-df_change['positive_percent_change_7d'] = df_change['percent_change_7d'] > 0
+df_change['positive_percentChange1h'] = df_change['percentChange1h'] > 0
+df_change['positive_percentChange24h'] = df_change['percentChange24h'] > 0
+df_change['positive_percentChange7d'] = df_change['percentChange7d'] > 0
 
 col1.dataframe(df_change)
 
@@ -125,40 +125,40 @@ col2.subheader('Bar plot of the percentage price change')
 
 if percentage_time_frame == '7d':
     if sort_by_value == 'Yes':
-        df_change = df_change.sort_values(by=['percent_change_7d'])
+        df_change = df_change.sort_values(by=['percentChange7d'])
     col2.write('*7 day period*')
     plt.figure(figsize=(5, 25))
     plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-    df_change['percent_change_7d'].plot(kind='barh',
-                                        color=df_change.positive_change_7d.map(
-                                            {
-                                                True: 'g',
-                                                False: 'r'
-                                            }))
+    df_change['percentChange7d'].plot(
+        kind='barh',
+        color=df_change.positive_percentChange7d.map({
+            True: 'g',
+            False: 'r'
+        }))
     col2.pyplot(plt)
 elif percentage_time_frame == '24h':
     if sort_by_value == 'Yes':
-        df_change = df_change.sort_values(by=['percent_change_24h'])
+        df_change = df_change.sort_values(by=['percentChange24h'])
     col2.write('*24 hour period*')
     plt.figure(figsize=(5, 25))
     plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-    df_change['percent_change_24h'].plot(
+    df_change['percentChange24h'].plot(
         kind='barh',
-        color=df_change.positive_change_24h.map({
+        color=df_change.positive_percentChange24h.map({
             True: 'g',
             False: 'r'
         }))
     col2.pyplot(plt)
 else:
     if sort_by_value == 'Yes':
-        df_change = df_change.sort_values(by=['percent_change_1h'])
+        df_change = df_change.sort_values(by=['percentChange1h'])
     col2.write('*1 hour period*')
     plt.figure(figsize=(5, 25))
     plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-    df_change['percent_change_1h'].plot(kind='barh',
-                                        color=df_change.positive_change_1h.map(
-                                            {
-                                                True: 'g',
-                                                False: 'r'
-                                            }))
+    df_change['percentChange1h'].plot(
+        kind='barh',
+        color=df_change.positive_percentChange1h.map({
+            True: 'g',
+            False: 'r'
+        }))
     col2.pyplot(plt)
